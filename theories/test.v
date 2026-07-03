67c73710178167d12bfe943f9017ec15b24140a2
Florent Hivert <florent.hivert@lisn.fr>
2026-07-03 23:13:58 +0200

    Correct docker image

--- theories/test.v
+++ theories/test.v
@@ -1,16 +1,15 @@
 From HB Require Import structures.
 From Stdlib Require Import Znat BinIntDef Uint63 Ring Ring63.
-From mathcomp Require Import all_boot all_order.
+From mathcomp Require Import boot order.
 From mathcomp Require Import nmodule ssralg zmodp countalg finalg.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
+Section Nat.
 
 Local Open Scope uint63_scope.
-
-Section Nat.
 
 Implicit Type (m n p q : nat).
 
